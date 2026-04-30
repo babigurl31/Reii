03fc1ba9b2b3aec4069c031d165d80d9d9ec52b7
babigurl31 <ttqnghi0331@gmail.com>
2026-04-30 18:19:55 +0700

    Update drawer.js

--- drawer.js
+++ drawer.js
@@ -1,3 +1,13 @@
 window.toggleDrawer = (drawerId) => {
-    document.getElementById(drawerId).classList.toggle('open');
+    const target = document.getElementById(drawerId);
+    // Tìm ngăn kéo đối diện
+    const otherId = drawerId === 'leftDrawer' ? 'rightDrawer' : 'leftDrawer';
+    const other = document.getElementById(otherId);
+
+    if (target.classList.contains('open')) {
+        target.classList.remove('open');
+    } else {
+        target.classList.add('open');
+        other.classList.remove('open'); // Ra lệnh đóng ngăn đối diện
+    }
 };
